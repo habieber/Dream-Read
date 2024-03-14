f9c9817100657d8b1c00f58c90fa5f6432d3de3a
Holly Bieber <habieber1108@gmail.com>
2024-03-14 14:55:48 -0500

    Initial commit

---
+++ models/book.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    datePublished: Date,
+    pageCount: Number,
+    dateAdded: { type: Date, default: new Date() },
+    priority: { 
+        type: String, 
+        default: 'low',
+        enum: ['low', 'med', 'high']
+    },
+    comments: [commentSchema],
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    userName: String,
+    userAvatar: String
+},
+{
+    timestamps: true
+});
+
+const commentSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    userName: String,
+    userAvatar: String
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Book', bookSchema);
