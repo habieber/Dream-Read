d2d7bd9c1a77ab9c1f0b178e9cece5346f219914
Holly Bieber <habieber1108@gmail.com>
2024-03-19 13:39:21 -0500

    Google Books API integration

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,24 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
+const ROOT_URL = 'https://www.googleapis.com/books/v1/volumes';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Dream Read' });
 });
+
+// GET results of search
+router.get('/books/list', async function(req, res, next) {
+  const bookTitle = req.query.volume
+  if (!bookTitle) return res.render('/', { title: 'Dream Read', bookData: null });
+  
+  const bookData = await fetch(`${ROOT_URL}?q=${bookTitle}`)
+    .then(res => res.json())
+
+    console.log(bookData)
+  res.render('books/list', { bookData, title: `${bookTitle}` })
+})
 
 router.get('/auth/google', passport.authenticate(
   'google',
